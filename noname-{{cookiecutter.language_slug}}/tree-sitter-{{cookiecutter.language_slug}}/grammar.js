3765baaa1f6082ac82f490dbaa5adda496da4484
Lachance, Alexandre <alexandrelachance@me.com>
2023-10-28 17:24:03 -0400

    Add language server and grammar templates.

---
+++ noname-{{cookiecutter.language_slug}}/tree-sitter-{{cookiecutter.language_slug}}/grammar.js
@@ -0,0 +1,8 @@
+module.exports = grammar({
+    name: '{{cookiecutter.language_name}}',
+
+    rules: {
+        // TODO: add the actual grammar rules
+        source_file: $ => 'hello',
+    }
+});
